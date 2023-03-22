09da1fcfe954fa18247138040928e775df766de9
george <zatzoid.gmail.com>
2023-03-22 17:52:32 +0300

    day3

--- js/edit-form.js
+++ js/edit-form.js
@@ -1,8 +1,23 @@
-let redactor = document.querySelector("profile__redactor");
-let popup = document.querySelector("popup");
-console.log(redactor);
+let popup = document.querySelector(".popup");
+let redactor = document.querySelector(".profile__redactor");
+let close = document.querySelector(".form__close");
+/* open */
+function openRedactor() {
+    console.log('button-ok');
+    popup.classList.add('popup_opened');
+    
+};
+redactor.addEventListener('click', openRedactor);
+/* close */
+function closeRedactor() {
+    console.log('close ok')
+popup.classList.remove('popup_opened')
 
-function openRedactor(){
-    redactor.addEventListener
+};
 
-}
+close.addEventListener('click', closeRedactor);
+/*  */
+
+let name = document.querySelector(".form__name");
+let status = document.querySelector(".form__status");
+
